ef98377b6e9a0649f5e6de53434718a82d4278c9
tathagatoroy <tathagatoroy3.gmail.com>
2021-09-20 02:54:02 +0530

    Transactional functions added,only hashing of string left

--- Assignments/1/contracts/Market.sol
+++ Assignments/1/contracts/Market.sol
@@ -21,6 +21,8 @@ contract Market {
         string item_name;
         string item_description;
         bool sold_or_withdrawn;
+        bool buyer_alloted;
+        address payable buyer;
         //address owner
     }
 
@@ -48,9 +50,12 @@ contract Market {
     /// @dev this event is for when listing is modified ,item sold or withdrawn
     event ListingChanged(address indexed seller, uint256 indexed index);
     /// @dev this event is for when item purchase is requested by buyer
-    event PurchaseRequested(uint256 indexed listing_id, address indexed buyer);
+    event PurchaseRequested(listings list, address indexed buyer);
     /// @dev this event is for when seller confirms item purchase by buyer
-    event encryptedKey(uint256 indexed listing_id, bytes32 indexed H);
+    /// @dev the hash of the item string is added the block chain from where the buyer can retrieve and decrypt
+    event encryptedKey(uint256 indexed listing_id, string indexed H);
+    /// event emitted when the a item is bought and both the seller and buyer gets the money/item
+    event PurchaseComplete(listings list);
 
     /// @dev this event is emitted when a sale is initiated
     // event SaleCreated(
@@ -87,7 +92,9 @@ contract Market {
             price,
             item_name,
             item_description,
-            false
+            false, 
+            false,
+            address(0)
         );
         // emit the update
         emit ListingCreated(
@@ -101,6 +108,7 @@ contract Market {
     }
 
     /* Returns all unsold/unwithdrawn market items */
+    ///@dev returns a list of active listings
     function fetchactivelistings() external view returns (listings[] memory) {
         uint256 currentIndex = 0;
 
@@ -117,6 +125,8 @@ contract Market {
     }
 
     //request from buyer to seller for item's purchase
+    // the contract emits a event to let the seller know that an buyer has been found
+    /// @dev listing id is the id of the item buyer is interested in
     function requestBuy(uint256 listing_id) external payable {
         require(
             listing_id < current_listing_id && listing_id >= 0,
@@ -126,11 +136,31 @@ contract Market {
             !Listings[listing_id].sold_or_withdrawn,
             "The item has already been bought"
         ); // Check if the item is still available
-        emit PurchaseRequested(listing_id, msg.sender);
+        require(
+          !Listings[listing_id].buyer_alloted,
+          "the item already has a buyer,in midst of transaction"
+        ); // Check whether item has a buyer already
+        require(
+          msg.value == 2*Listings[listing_id].price,
+          "The required deposit for the purchase not given "
+        );// check whether you have provided 2 times the selling price,for security purposes
+        require(
+          msg.sender != Listings[listing_id].seller,
+          "You cant buy your own items "
+        );
+        //Check that the seller is not the buyer
+
+        // let the seller know you have found a buyer
+        Listings[listing_id].buyer = msg.sender;
+        Listings[listing_id].buyer_alloted = true;
+        emit PurchaseRequested(Listings[listing_id], msg.sender);
     }
 
     //Sale of item from seller's side
-    function sellItem(uint256 listing_id, bytes32 H) external payable {
+    // transaction from the seller 
+    /// @dev listing id is the id of the item being sold_
+    /// @dev H is the hashed key for the item string (Hashed using the public key of the buyer off-chain)
+    function sellItem(uint256 listing_id,string calldata  H) external payable {
         require(
             listing_id < current_listing_id && listing_id >= 0,
             "Listing id is invalid"
@@ -139,10 +169,41 @@ contract Market {
             !Listings[listing_id].sold_or_withdrawn,
             "The item has already been bought"
         ); // Check if the item is still available
-        Listings[listing_id].sold_or_withdrawn = true;
+        require(
+          msg.sender == Listings[listing_id].seller,
+          "This is not your listing you cant sell this "
+        );
+        // check whether the caller is the seller
+        //Listings[listing_id].sold_or_withdrawn = true;
+
         emit encryptedKey(listing_id, H);
     }
 
     //Confirmation from buyer on receiving item
-    function confirmDelivery(uint256 listing_id) external payable {}
+    // the buyer confirms to get the refund of the money owed.(price)
+    // the seller gets his amount + price (3 * price)
+    /// @dev the listing id is the item being exchanged
+    function confirmDelivery(uint256 listing_id) external payable {
+    require(
+            listing_id < current_listing_id && listing_id >= 0,
+            "Listing id is invalid"
+        ); // Check for valid listing id
+        require(
+            !Listings[listing_id].sold_or_withdrawn,
+            "The item has already been bought"
+        ); // Check if the item is still available
+        require(
+          msg.sender == Listings[listing_id].buyer,
+          "You are not the buyer for this item "
+        );
+        // check whether buyer is the messenger
+        //refund the seller
+        Listings[listing_id].sold_or_withdrawn = true;
+        Listings[listing_id].seller.transfer(3*Listings[listing_id].price);
+        // refund the buyer
+        Listings[listing_id].buyer.transfer(Listings[listing_id].price);
+        emit PurchaseComplete(Listings[listing_id]);
+
+
+    }
 }
