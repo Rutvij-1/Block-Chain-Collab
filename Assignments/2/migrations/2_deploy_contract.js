6fcd1eb3c92dc864269686a37f86992e354c2aa9
tathagatoroy <42072694+tathagatoroy@users.noreply.github.com>
2021-10-10 04:19:33 +0530

    Updated migration.js

--- Assignments/2/migrations/2_deploy_contract.js
+++ Assignments/2/migrations/2_deploy_contract.js
@@ -1,9 +1,16 @@
 var SimpleStorage = artifacts.require("./SimpleStorage.sol");
 var Market = artifacts.require("./Market.sol");
 var VikreyAuction = artifacts.require("./VikreyAuction.sol");
+var VikreyAuction2 = artifacts.require("./VikreyAuction2.sol");
+var BlindAuction = artifacts.require("./BlindAuction.sol");
 
 module.exports = function (deployer, network, accounts) {
   deployer.deploy(SimpleStorage);
   deployer.deploy(Market);
+  deployer.deploy(BlindAuction);
+  deployer.deploy(VikreyAuction2);
   deployer.deploy(VikreyAuction, 0, 100, 100, accounts[0]);
+
 };
+
+
