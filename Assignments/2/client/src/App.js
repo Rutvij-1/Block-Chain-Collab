5a4d1dd108777c02096fd72916c427d9f5ae73cf
snehalkumar5 <snehal14201@gmail.com>
2021-10-10 14:10:53 +0400

    form

--- Assignments/2/client/src/App.js
+++ Assignments/2/client/src/App.js
@@ -1,15 +1,14 @@
 import React, { Component } from "react";
-import Market from "./contracts/Market.json";
-import VikreyAuction from "./contracts/VikreyAuction.json"
+import VickreyAuction2 from "./contracts/VickreyAuction2.json"
 import getWeb3 from "./getWeb3";
 import NavBar from "./components/navbar";
-// import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import MarketPlace from "./components/listmarket";
 import "./App.css";
 
 class App extends Component {
   constructor(props){
     super(props)
+
     this.state = { 
     initialised: false,
     web3: null, 
@@ -17,12 +16,16 @@ class App extends Component {
     currentAccount: null,
     contract: null,
     listings: [],
-    showlistings: false 
+    showlistings: false,
+    formData: {}
   };
+
   this.activeListings = this.activeListings.bind(this);
   this.init = this.init.bind(this);
   this.cancelAuction = this.cancelAuction.bind(this);
   this.onClickBid = this.onClickBid.bind(this);
+  this.handleChange = this.handleChange.bind(this);
+  this.createAuction = this.createAuction.bind(this);
 }
 
   componentDidMount = async () => {
@@ -35,14 +38,13 @@ class App extends Component {
       console.log(accounts);
       // Get the contract instance.
       const networkId = await web3.eth.net.getId();
-      const deployedNetwork = Market.networks[networkId];
+      const deployedNetwork = VickreyAuction2.networks[networkId];
       const instance = await new web3.eth.Contract(
-        Market.abi,
+        VickreyAuction2.abi,
         deployedNetwork && deployedNetwork.address,
       );
       console.log(accounts,deployedNetwork.address);
       instance.options.address = deployedNetwork.address
-      // 0x456160eF0d7E0bE15aDDeeDb4d90cA6d8599C825
       // Set web3, accounts, and contract to the state, and then proceed with an
       // example of interacting with the contract's methods.
       this.setState({ web3, accounts, contract: instance, initialised: true, cur_account: accounts[0] }, this.init);
@@ -60,10 +62,10 @@ class App extends Component {
     return
     const { accounts, contract, web3 } = this.state;
 
-    // await contract.methods.createListings(200,"Book",
-		// 	"Harry Potter and the Philosopher's Stone"
+    // await contract.methods.auctionItem("Book",
+		// 	"Harry Potter and the Philosopher's Stone",100,10000
 		// ).send({ from: accounts[0] });
-		await contract.methods.createListings(201100, "Mobile Phone", "One Plus 5T").send({from:accounts[0]});
+		// await contract.methods.auctionItem("Mobile Phone", "One Plus 5T",100,111010).send({from:accounts[0]});
 		// await contract.methods.createListings(20000, "Mobile Phone", "One Plus 5T").send({ from: accounts[0] });
 		// await contract.methods.createListings(3500,"Netflix screen","3 Screens, FHD").send({ from: accounts[0] });
     // await contract.methods.set(10).send({ from: accounts[0] });
@@ -76,20 +78,13 @@ class App extends Component {
   };
 
   activeListings = async() => {
-    const { accounts, contract } = this.state;
+    const { accounts, contract, showlistings } = this.state;
     console.log(accounts);
-		// await contract.methods.createListings(20000, "Mobile Phone", "One Plus 5T").send({from:accounts[0]})
-    // .then(res=>{
-    //   console.log(res);
-    // },function(err){
-    //   console.log(`error ${err}`);
-    // });
-
 		// await contract.methods.createListings(20010, "Mobile Phone", "One Plus 5T");
 		// await contract.methods.createListings(20011, "Mobile Phone", "One Plus 5T").call();
-    let temp = await contract.methods.fetchactivelistings().call();
+    let temp = await contract.methods.getactiveauctions().call();
     console.log(temp);
-    this.setState({ listings:temp, showlistings:true });
+    this.setState({ listings:temp, showlistings:!showlistings });
   };
 
   onClickBid = async() => {
@@ -99,6 +94,20 @@ class App extends Component {
 
   };
 
+  createAuction(e) {
+    e.preventDefault();
+    const { accounts, contract } = this.state;
+    const { item_name,item_description,bidding_time,reveal_time } = this.state.formData;
+    console.log(this.state.formData);
+    contract.methods.auctionItem(item_name, item_description, bidding_time, reveal_time).send({ from: accounts[0] });
+  };
+
+  handleChange(e) {
+    e.preventDefault();
+    const formData = Object.assign({}, this.state.formData);
+    formData[e.target.id] = e.target.value;
+    this.setState({ formData: formData });
+}
   render() { 
     if (!this.state.web3) {
       return <div>Loading Web3, accounts, and contract...</div>;
@@ -109,49 +118,52 @@ class App extends Component {
         {/* <NavBar/> */}
         <h1>Smart Contract - Auction</h1>
         <button onClick={this.activeListings}>
-          Active Listings
+          Show Active Listings
         </button>
         {this.state.showlistings ?
         <>
-        <div>The active listings are: {this.state.listings}</div>
+        <div>The active listings are:</div>
         <table>
             <thead>
                 <tr>
-                    <td>Listing ID</td>
-                    <td>Seller</td>
-                    <td>Buyer</td>
-                    <td>Price</td>
+                    <td>Auction ID</td>
                     <td>Item Name</td>
                     <td>Item Description</td>
+                    <td>biddingEnd</td>
+                    <td>revealEnd</td>
                     <td>Sold</td>
-                    <td>Buyer Alloted</td>
+                    {/* <td>Seller</td>
+                    <td>Buyer</td>
+                    <td>Price</td>
+                    <td>Buyer Alloted</td> */}
                     <td>Status</td>
-                    <td>Start</td>
+                    <td>Bid</td>
                 </tr>
             </thead>
             <tbody>
             {this.state.listings.map(listing => {
                 let status = 'Active'
-                if (listing.sold_or_withdrawn) {
-                    status = 'Canceled'
+                if (listing.ended) {
+                    status = 'Ended'
                 } 
                 return (
-                    <tr key={listing.id}>
-                        <td>{listing.seller}</td>
-                        <td>{listing.buyer}</td>
-                        <td>{listing.price} ETH</td>
-                        <td>{listing.item_name} ETH</td>
-                        <td>{listing.sold_or_withdrawn} ETH</td>
-                        <td>{listing.buyer_alloted}</td>
+                    <tr key={listing.auction_id}>
+                        <td>{listing.auction_id}</td>
+                        <td>{listing.item_name}</td>
+                        <td>{listing.item_description}</td>
+                        <td>{listing.biddingEnd}</td>
+                        <td>{listing.revealEnd} ETH</td>
+                        <td>{listing.ended} ETH</td>
                         <td>{status}</td>
                         <td>
-                            {listing.seller == this.state.currentAccount && (status === 'Active' || status === 'Unstarted') &&
+                            {listing.owner == this.state.currentAccount && (status === 'Active' || status === 'Unstarted') ?
                                 <button onClick={() => this.cancelAuction(listing)}>Cancel</button>
-                            }
+                                :
                             <div>
                                 <input ref={x => this._inputBidAmount = x} />
                                 <button onClick={() => this.onClickBid(listing)}>Bid</button>
                             </div>
+                          }
                         </td>
                     </tr>
                 )
@@ -161,27 +173,28 @@ class App extends Component {
         </>
         :
          <div>
-                <h1>Auctions</h1>
-                <div className="form-create-auction">
-                    <h2>Create auction</h2>
-                    {/* <div>
-                        Reserve <input type="text" ref={x => this._inputReserve = x} defaultValue={0} />
-                    </div> */}
-                    <div>
-                        Item<input type="text" ref={x => this.state.item_desc = x} defaultValue={100000000000000000} />
-                    </div>
-                    <div>
-                        Owner <input type="text" ref={x => this.state.beneficiary = x} defaultValue={0} />
-                    </div>
-                    <div>
-                        Ended block <input type="text" ref={x => this.state.ended = x} defaultValue={10} />
-                    </div>
-                    <button onClick={this.onClickCreateAuction}>Create Auction</button>
+            <h1>Auctions</h1>
+            <div className="form-create-auction">
+              <form onSubmit={this.createAuction}>
+                <h2>Create auction</h2>
+                <div>
+                    Item Name <input type="item_name" className="form-control" id="item_name" required onChange={this.handleChange} defaultValue={"Item Name"} />
+                    {/* Item<input type="text" ref={x => this.state.item_desc = x} defaultValue={100000000000000000} /> */}
                 </div>
+                <div>
+                    Item description <input type="item_description" className="form-control" id="item_description" required onChange={this.handleChange} defaultValue={"Item description"} />
+                </div>
+                <div>
+                    Bidding Time <input type="bidding_time" className="form-control" id="bidding_time" required onChange={this.handleChange} defaultValue={100} />
+                </div>
+                <div>
+                Reveal Time <input type="reveal_time" className="form-control" id="reveal_time" required onChange={this.handleChange} defaultValue={100} />
+                </div>
+                <button type="submit">Create Auction</button>
+              </form>
             </div>
+          </div>
         }
-        {/* <MarketPlace web3 = {this.state.web3}/> */}
-        {/* <div>The stored value is: {this.state.storageValue}</div> */}
       </div>
     );
   }
