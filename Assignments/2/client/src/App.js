5b851c094a9afe1d17005456aa08d207ef42c582
snehalkumar5 <snehal14201@gmail.com>
2021-10-10 11:35:47 +0400

    tble

--- Assignments/2/client/src/App.js
+++ Assignments/2/client/src/App.js
@@ -1,5 +1,4 @@
 import React, { Component } from "react";
-import SimpleStorageContract from "./contracts/SimpleStorage.json";
 import Market from "./contracts/Market.json";
 import VikreyAuction from "./contracts/VikreyAuction.json"
 import getWeb3 from "./getWeb3";
@@ -7,22 +6,23 @@ import NavBar from "./components/navbar";
 // import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import MarketPlace from "./components/listmarket";
 import "./App.css";
-// var contract = require("@truffle/contract");
 
 class App extends Component {
   constructor(props){
     super(props)
     this.state = { 
     initialised: false,
-    storageValue: 0, 
     web3: null, 
     accounts: null, 
-    cur_account: null,
+    currentAccount: null,
     contract: null,
-    listings: [] 
+    listings: [],
+    showlistings: false 
   };
   this.activeListings = this.activeListings.bind(this);
   this.init = this.init.bind(this);
+  this.cancelAuction = this.cancelAuction.bind(this);
+  this.onClickBid = this.onClickBid.bind(this);
 }
 
   componentDidMount = async () => {
@@ -40,9 +40,9 @@ class App extends Component {
         Market.abi,
         deployedNetwork && deployedNetwork.address,
       );
-      console.log(accounts);
-      // instance.options.address = `${accounts[0]}`
-
+      console.log(accounts,deployedNetwork.address);
+      instance.options.address = deployedNetwork.address
+      // 0x456160eF0d7E0bE15aDDeeDb4d90cA6d8599C825
       // Set web3, accounts, and contract to the state, and then proceed with an
       // example of interacting with the contract's methods.
       this.setState({ web3, accounts, contract: instance, initialised: true, cur_account: accounts[0] }, this.init);
@@ -63,12 +63,7 @@ class App extends Component {
     // await contract.methods.createListings(200,"Book",
 		// 	"Harry Potter and the Philosopher's Stone"
 		// ).send({ from: accounts[0] });
-		await contract.methods.createListings(201100, "Mobile Phone", "One Plus 5T").call({from:accounts[0]});
-    // .then(res=>{
-    //   console.log(res);
-    // },function(err){
-    //   console.log(`error ${err}`);
-    // });
+		await contract.methods.createListings(201100, "Mobile Phone", "One Plus 5T").send({from:accounts[0]});
 		// await contract.methods.createListings(20000, "Mobile Phone", "One Plus 5T").send({ from: accounts[0] });
 		// await contract.methods.createListings(3500,"Netflix screen","3 Screens, FHD").send({ from: accounts[0] });
     // await contract.methods.set(10).send({ from: accounts[0] });
@@ -82,27 +77,26 @@ class App extends Component {
 
   activeListings = async() => {
     const { accounts, contract } = this.state;
-    // console.log(accounts);
-		await contract.methods.createListings(20000, "Mobile Phone", "One Plus 5T").send({from:accounts[0]})
-    .then(res=>{
-      console.log(res);
-    },function(err){
-      console.log(`error ${err}`);
-    });
-
-		await contract.methods.createListings(20010, "Mobile Phone", "One Plus 5T");
-		// await contract.methods.createListings(20011, "Mobile Phone", "One Plus 5T").call();
-    let temp = await contract.methods.fetchactivelistings().call();
+    console.log(accounts);
+		// await contract.methods.createListings(20000, "Mobile Phone", "One Plus 5T").send({from:accounts[0]})
     // .then(res=>{
     //   console.log(res);
     // },function(err){
     //   console.log(`error ${err}`);
     // });
-    console.log(temp);
 
-    // send({ from: this.state.accounts[0] });
-    // console.log(listings);
-    // this.setState({ listings });
+		// await contract.methods.createListings(20010, "Mobile Phone", "One Plus 5T");
+		// await contract.methods.createListings(20011, "Mobile Phone", "One Plus 5T").call();
+    let temp = await contract.methods.fetchactivelistings().call();
+    console.log(temp);
+    this.setState({ listings:temp, showlistings:true });
+  };
+
+  onClickBid = async() => {
+
+  };
+  cancelAuction = async() => {
+
   };
 
   render() { 
@@ -117,8 +111,56 @@ class App extends Component {
         <button onClick={this.activeListings}>
           Active Listings
         </button>
+        {this.state.showlistings ?
+        <>
         <div>The active listings are: {this.state.listings}</div>
-        <div>
+        <table>
+            <thead>
+                <tr>
+                    <td>Listing ID</td>
+                    <td>Seller</td>
+                    <td>Buyer</td>
+                    <td>Price</td>
+                    <td>Item Name</td>
+                    <td>Item Description</td>
+                    <td>Sold</td>
+                    <td>Buyer Alloted</td>
+                    <td>Status</td>
+                    <td>Start</td>
+                </tr>
+            </thead>
+            <tbody>
+            {this.state.listings.map(listing => {
+                let status = 'Active'
+                if (listing.sold_or_withdrawn) {
+                    status = 'Canceled'
+                } 
+                return (
+                    <tr key={listing.id}>
+                        <td>{listing.seller}</td>
+                        <td>{listing.buyer}</td>
+                        <td>{listing.price} ETH</td>
+                        <td>{listing.item_name} ETH</td>
+                        <td>{listing.sold_or_withdrawn} ETH</td>
+                        <td>{listing.buyer_alloted}</td>
+                        <td>{status}</td>
+                        <td>
+                            {listing.seller == this.state.currentAccount && (status === 'Active' || status === 'Unstarted') &&
+                                <button onClick={() => this.cancelAuction(listing)}>Cancel</button>
+                            }
+                            <div>
+                                <input ref={x => this._inputBidAmount = x} />
+                                <button onClick={() => this.onClickBid(listing)}>Bid</button>
+                            </div>
+                        </td>
+                    </tr>
+                )
+            })}
+            </tbody>
+        </table>
+        </>
+        :
+         <div>
                 <h1>Auctions</h1>
                 <div className="form-create-auction">
                     <h2>Create auction</h2>
@@ -137,6 +179,7 @@ class App extends Component {
                     <button onClick={this.onClickCreateAuction}>Create Auction</button>
                 </div>
             </div>
+        }
         {/* <MarketPlace web3 = {this.state.web3}/> */}
         {/* <div>The stored value is: {this.state.storageValue}</div> */}
       </div>
