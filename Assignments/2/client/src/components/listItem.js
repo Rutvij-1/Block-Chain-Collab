bdb8ab67ce4af06e0ca52b4c6d8c68c7fe1f1c33
snehalkumar5 <snehal14201@gmail.com>
2021-10-10 22:22:17 +0400

    frontend changes

---
+++ Assignments/2/client/src/components/listItem.js
@@ -0,0 +1,44 @@
+import React, { Component } from 'react'
+import { Button, InputGroup } from 'react-bootstrap';
+
+class ListAuctionItem extends Component {
+    constructor(props){
+        super(props)
+    }
+    render() {
+        return(
+            <div className="form-group">
+                <form onSubmit={this.props.handleSubmit}>
+                <h2>Create auction</h2>
+                <div className="mb-3">
+                <label className="form-label">Item Name</label>
+                    <input type="item_name" className="form-control" id="item_name" required onChange={this.props.handleChange} placeholder="Book" />
+                </div>
+                <div className="mb-3">
+                    <label className="form-label">Item description</label>
+                    <input type="item_description" className="form-control" id="item_description" required onChange={this.props.handleChange} placeholder="Harry Potter" />
+                </div>
+                <div className="mb-3">
+                    <label className="form-label">Bidding Time</label>
+                    <input type="bidding_time" className="form-control" id="bidding_time" required onChange={this.props.handleChange} placeholder="100 (in seconds)"/>
+                </div>
+                <div className="mb-3">
+                    <label className="form-label">Reveal Time</label>
+                    <input type="reveal_time" className="form-control" id="reveal_time" required onChange={this.props.handleChange} placeholder="120 (in seconds)"/>
+                </div>
+                <div className="mb-3">
+                    <label className="form-label">Auction Type</label>
+                    <select className="form-select" id="auctionType" placeholder="Select Auction Type" required onChange={this.props.handleChange}>
+                    <option value="Blind Auction" selected>Blind Auction</option>
+                    <option value="Vickrey Auction">Vickrey Auction</option>
+                    <option value="Average Price Auction">Average Price Auction</option>
+                    </select>
+                </div>
+                <br/>
+                <Button type="submit">Create Auction</Button>
+                </form>
+            </div>
+        );
+    }
+}
+export default ListAuctionItem;
--- Assignments/2/client/src/components/listmarket.js
+++ Assignments/2/client/src/components/listmarket.js
@@ -1,58 +1,104 @@
 import React, { Component } from 'react'
+import { Table, Button, InputGroup } from 'react-bootstrap';
 
 class MarketPlace extends Component {
-    constructor(props){
-        super(props)
-        this.state = {
-            currentAccount :  '',
-            currentAccountBalance : 0,
-            items : [],
-            auctions : []
-        }
+  constructor(props){
+    super(props)
+    this.state = {
+      currentAccount :  '',
+      listings : [],
+      makebid: false,
+      formData: {}
     }
-    render() {
-        return (
-          <div id="content">
-            <form onSubmit={(event) => {
-              event.preventDefault()
-              this.props.createTask(this.task.value)
-            }}>
-              <input
-                id="newTask"
-                ref={(input) => {
-                  this.task = input
-                }}
-                type="text"
-                className="form-control"
-                placeholder="Add task..."
-                required />
-              <input type="submit" hidden={true} />
-            </form>
-            <ul id="taskList" className="list-unstyled">
-              { this.props.tasks.map((task, key) => {
-                return(
-                  <div className="taskTemplate" className="checkbox" key={key}>
-                    <label>
-                      <input
-                      type="checkbox"
-                      name={task.id}
-                      defaultChecked={task.completed}
-                      ref={(input) => {
-                        this.checkbox = input
-                      }}
-                      onClick={(event) => {
-                        this.props.toggleCompleted(this.checkbox.name) }}/>
-                      <span className="content">{task.content}</span>
-                    </label>
-                  </div>
-                )
-              })}
-            </ul>
-            <ul id="completedTaskList" className="list-unstyled">
-            </ul>
-          </div>
-        );
-      }
-    }
+    this.handleChange = this.handleChange.bind(this);
+    this.makeBid = this.makeBid.bind(this);
     
-    export default MarketPlace;
+  }
+    componentDidMount = async () => {
+    try {
+        let temp = await this.props.vickrey_contract.methods.getactiveauctions().call();
+        let ret = await this.props.blind_contract.methods.getactiveauctions().call();
+        temp = temp.concat(ret)
+        this.setState({ listings:temp, currentAccount: this.props.account });
+
+    } catch (error) {
+        alert(`Loading...`);
+        console.error(error);
+      }
+    };
+  makeBid(e){
+    e.preventDefault();
+    const {deposit, secret} = this.state.formData
+    this.setState({ makebid: !this.state.makebid})
+  }
+  
+  handleChange(e) {
+    e.preventDefault();
+    const formData = Object.assign({}, this.state.formData);
+    formData[e.target.id] = e.target.value;
+    this.setState({ formData: formData });
+  };
+  render() {
+    return (
+      <>
+    <h2>The active listings are:</h2>
+    <div style={{
+      display: "flex",
+      justifyContent: "center",
+      alignItems: "center",
+    }}>
+    <Table striped bordered hover>
+      <thead>
+        <tr>
+          <td>Auction ID</td>
+          <td>Item Name</td>
+          <td>Item Description</td>
+          <td>Bidding Time</td>
+          <td>Bid Reveal Time</td>
+          {/* <td>Sold</td> */}
+          {/* <td>Status</td> */}
+          <td>Manage</td>
+        </tr>
+      </thead>
+      <tbody>
+        {this.state.listings.map(listing => {
+          let status = 'Active'
+          let sold = "False"
+          if (listing.ended) {
+              status = 'Ended'
+              sold = "True"
+          } 
+          return (
+            <tr key={listing.auction_id}>
+              <td>{listing.auction_id}</td>
+              <td>{listing.item_name}</td>
+              <td>{listing.item_description}</td>
+              <td>{listing.biddingEnd}</td>
+              <td>{listing.revealEnd}</td>
+              {/* <td>{sold}</td> */}
+              {/* <td>{status}</td> */}
+              <td>
+                {listing.owner === this.state.currentAccount && (status === 'Active' || status === 'Unstarted') ?
+                    <Button onClick={() => this.cancelAuction(listing)}>Cancel</Button>
+                    :
+                  <div>
+                    <InputGroup>
+                    <input type="number" className="form-control" id="deposit" required onChange={this.handleChange} placeholder="Bid Amount" />
+                    <input type="password" className="form-control" id="secret_key" required onChange={this.handleChange} placeholder="Secret Key" />
+                    </InputGroup>
+                    <Button className="success" onClick={this.placeBid}>Place Bid</Button>
+                  </div>
+                }
+              </td>
+            </tr>
+          )
+      })}
+      </tbody>
+    </Table>
+    </div>
+    </>
+    );
+  }
+}
+    
+export default MarketPlace;
