532bb65505566b9277ab0f0f21e22967eb6f97f2
Rutvij-1 <menavlikarrutvij@gmail.com>
2021-10-10 12:50:08 +0530

    Migration issue fixed

--- Assignments/2/migrations/2_deploy_contract.js
+++ Assignments/2/migrations/2_deploy_contract.js
@@ -1,16 +1,14 @@
-var SimpleStorage = artifacts.require("./SimpleStorage.sol");
-var Market = artifacts.require("./Market.sol");
-var VikreyAuction = artifacts.require("./VikreyAuction.sol");
-var VikreyAuction2 = artifacts.require("./VikreyAuction2.sol");
-var BlindAuction = artifacts.require("./BlindAuction.sol");
+var SimpleStorage = artifacts.require("SimpleStorage.sol");
+var Market = artifacts.require("Market.sol");
+var VikreyAuction = artifacts.require("VikreyAuction.sol");
+var VickreyAuction2 = artifacts.require("VickreyAuction2.sol");
+var BlindAuction = artifacts.require("BlindAuction.sol");
 
 module.exports = function (deployer, network, accounts) {
-  deployer.deploy(SimpleStorage);
-  deployer.deploy(Market);
-  deployer.deploy(BlindAuction);
-  deployer.deploy(VikreyAuction2);
-  deployer.deploy(VikreyAuction, 0, 100, 100, accounts[0]);
+    deployer.deploy(SimpleStorage);
+    deployer.deploy(Market);
+    deployer.deploy(BlindAuction);
+    deployer.deploy(VickreyAuction2);
+    deployer.deploy(VikreyAuction, 0, 100, 100, accounts[0]);
 
 };
-
-
