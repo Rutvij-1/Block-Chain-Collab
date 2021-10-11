d8c99dbc37a53ad5b41a9d42f3290188a9284356
tathagatoroy <42072694+tathagatoroy@users.noreply.github.com>
2021-10-11 18:34:13 +0530

    Added Average Price Auction

--- Assignments/2/migrations/2_deploy_contract.js
+++ Assignments/2/migrations/2_deploy_contract.js
@@ -3,6 +3,8 @@ var Market = artifacts.require("Market.sol");
 var VikreyAuction = artifacts.require("VikreyAuction.sol");
 var VickreyAuction2 = artifacts.require("VickreyAuction2.sol");
 var BlindAuction = artifacts.require("BlindAuction.sol");
+var AveragePriceAuction = artifacts.require("./AveragePriceAuction.sol");
+
 
 module.exports = function (deployer, network, accounts) {
     // deployer.deploy(SimpleStorage);
@@ -10,5 +12,7 @@ module.exports = function (deployer, network, accounts) {
     deployer.deploy(BlindAuction);
     deployer.deploy(VickreyAuction2);
     deployer.deploy(VikreyAuction, 0, 100, 100, accounts[0]);
+    deployer.deploy(AveragePriceAuction);
+
 
 };
